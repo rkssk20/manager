fae75d803fd59eba2911779925e267c0f83f6dd4
ささき <castanie96@gmail.com>
2021-07-05 19:11:39 +0900

    update

--- server/app.js
+++ server/app.js
@@ -34,6 +34,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.audience.cf");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
