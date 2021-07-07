78d8f41f760e4082f8e3e07a9ca58e4aca569ed9
ささき <castanie96@gmail.com>
2021-07-08 01:59:45 +0900

    update

--- server/routes/likeRanking.js
+++ server/routes/likeRanking.js
@@ -44,7 +44,7 @@ router.get('/', async function(req, res){
           day = day + 7;
           Query();
         }else{
-          res.send(...resultList);
+          res.send({statusCode: 200, body: JSON.stringify(...resultList)});
         }
       });
     };
