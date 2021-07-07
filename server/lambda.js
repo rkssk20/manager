ca6fa8662cfee7ef551c206e20518a560b9fd496
ささき <castanie96@gmail.com>
2021-07-08 03:35:47 +0900

    update

--- server/lambda.js
+++ server/lambda.js
@@ -3,4 +3,10 @@ const app = require('./app');
 
 const server = serverlessExpress.createServer(app);
 
-exports.handler = (event, context) => serverlessExpress.proxy(server, event, context);
+exports.handler = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(event)
+  })
+  serverlessExpress.proxy(server, event, context);
+};
