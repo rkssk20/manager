25f6ea6b0d5321ceb14484776c279d9ec7a7b408
ささき <castanie96@gmail.com>
2021-07-08 18:04:37 +0900

    update

--- server/lambda.js
+++ server/lambda.js
@@ -21,6 +21,29 @@ const binaryMimeTypes = [
   'text/xml'
 ];
 
-serverlessExpress({app, resolutionMode: 'CALLBACK'});
+serverlessExpress({
+  app,
+  eventSource: {
+    getRequest: alb.getRequest,
+    getResponse: ({
+        statusCode,
+        body,
+        headers,
+        isBase64Encoded
+    }) => {
+        const multiValueHeaders = {};
+        Object.entries(headers).forEach(([headerKey, headerValue]) => {
+            const headerArray = Array.isArray(headerValue) ? headerValue : [String(headerValue)];
+            multiValueHeaders[headerKey.toLowerCase()] = headerArray;
+        });
+        return {
+            statusCode,
+            body,
+            multiValueHeaders,
+            isBase64Encoded
+        }
+    }
+  }
+});
 
 // exports.handler = (event, context) => serverlessExpress.proxy(server, event, context);
