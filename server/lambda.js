391f2cd6de96afce57e0fc59ff12136cfb33129a
ささき <castanie96@gmail.com>
2021-07-08 09:04:48 +0900

    update

--- server/lambda.js
+++ server/lambda.js
@@ -3,4 +3,27 @@ const app = require('./app');
 
 const server = serverlessExpress.createServer(app);
 
-exports.handler = (event, context) => serverlessExpress.proxy(server, event, context);
+exports.handler = serverlessExpress.proxy({
+  server,
+  eventSource: {
+    getRequest: alb.getRequest,
+    getResponse: ({
+      statusCode,
+      body,
+      headers,
+      isBase64Encoded
+    }) => {
+      const multiValueHeaders = {};
+      Object.entries(headers).forEach(([headerKey, headerValue]) => {
+        const headerArray = Array.isArray(headerValue) ? headerValue : [String(headerValue)];
+        multiValueHeaders[headerKey.toLowerCase()] = headerArray;
+      });
+      return {
+        statusCode,
+        body,
+        multiValueHeaders,
+        isBase64Encoded
+      }
+    }
+  }
+});
