dd756169098cad0f3c7a866452cedb57b5b799e2
ささき <castanie96@gmail.com>
2021-07-08 19:46:07 +0900

    update

--- server/lambda.js
+++ server/lambda.js
@@ -1,30 +1,8 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./app');
 
-const binaryMimeTypes = [
-  'application/javascript',
-  'application/json',
-  'application/octet-stream',
-  'application/xml',
-  'font/eot',
-  'font/opentype',
-  'font/otf',
-  'image/jpeg',
-  'image/png',
-  'image/svg+xml',
-  'text/comma-separated-values',
-  'text/css',
-  'text/html',
-  'text/javascript',
-  'text/plain',
-  'text/text',
-  'text/xml'
-];
+const server = serverlessExpress.createServer(app);
 
-console.log({'app': app})
-
-const server = serverlessExpress.createServer(app, binaryMimeTypes);
-
-console.log({'server': server})
+console.log({'app': app});
 
 exports.handler = (event, context) => serverlessExpress.proxy(server, event, context);
