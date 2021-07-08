0a626ef717dd3d4dc9970ad22db0c75e21c1d6fc
ささき <castanie96@gmail.com>
2021-07-08 17:17:32 +0900

    update

--- server/lambda.js
+++ server/lambda.js
@@ -1,29 +1,31 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./app');
 
-// const binaryMimeTypes = [
-//   'application/javascript',
-//   'application/json',
-//   'application/octet-stream',
-//   'application/xml',
-//   'font/eot',
-//   'font/opentype',
-//   'font/otf',
-//   'image/jpeg',
-//   'image/png',
-//   'image/svg+xml',
-//   'text/comma-separated-values',
-//   'text/css',
-//   'text/html',
-//   'text/javascript',
-//   'text/plain',
-//   'text/text',
-//   'text/xml'
-// ];
+const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+];
 
-const server = serverlessExpress.createServer(app, null);
+// const server = serverlessExpress.createServer(app, null);
 
-exports.handler = (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return awsServerlessExpress.proxy(server, event, context,'PROMISE').promise
-};
+exports.handler = serverlessExpress({
+  app,
+  binaryMimeTypes
+}).handler;
+
+// exports.handler = (event, context) => serverlessExpress.proxy(server, event, context);
