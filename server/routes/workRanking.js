81b5fb7739b981d4eb202679022efcf745dc3a38
ささき <castanie96@gmail.com>
2021-07-09 23:26:23 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-// 一週間以内の投稿から、レビューされた数の多い作品TOP3を取得する
-// 一週間以内の投稿が3件未満の場合、さらに一週間前から取得。これを3件たまるまで繰り返す。
+// 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
 router.get('/', async function(req, res){
 
   pool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1', function(error, result){
@@ -11,12 +10,12 @@ router.get('/', async function(req, res){
       throw error;
     };
 
-    console.log(result)
+    console.log(result[0])
 
     res.send({
       "statusCode": 200,
-      "body": JSON.stringify(result)
-    })
+      "body": JSON.stringify(result[0])
+    });
 
     // pool.query(
     //   `SELECT
@@ -45,51 +44,6 @@ router.get('/', async function(req, res){
     //   }
     // )
   });
-
-  // const resultList = [];
-  // let day = 0;
-  // const promisePool = pool.promise();
-
-  // try{
-  //   function Query(){
-  //     async function Loop(){      
-  //       const [rows, fields] = await promisePool.query(
-  //         `SELECT
-  //           works.work_id,
-  //           works.title,
-  //           works.genru,
-  //           works.name,
-  //           works.image,
-  //           COUNT(*) AS COUNT
-  //         FROM reviews          
-  //         INNER JOIN works ON reviews.work_id = works.work_id
-  //         WHERE reviews.created_at BETWEEN NOW() - INTERVAL ${ day + 7 } DAY AND NOW() - INTERVAL ${ day } DAY
-  //         GROUP BY reviews.work_id
-  //         ORDER BY COUNT DESC
-  //         LIMIT 3`
-  //       );
-
-  //       await resultList.push(rows);
-  //       return rows.length;
-  //     };
-
-  //     Loop().then((result) => {
-  //       if(result < 3){
-  //         day = day + 7;
-  //         Query();
-  //       }else{
-  //         res.send({
-  //           "statusCode": 200,
-  //           "body": JSON.stringify(...resultList)
-  //         });
-  //       }
-  //     });
-  //   };
-
-  //   Query();
-  // }catch(error){
-  //   console.log(error);
-  // }
 });
 
 module.exports = router;
