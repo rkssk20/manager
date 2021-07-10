95c9068ba26f941141701eeb14799be1fb7f94e4
ささき <castanie96@gmail.com>
2021-07-10 22:23:57 +0900

    update

--- server/lambda.js
+++ server/lambda.js
@@ -1,10 +1,12 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./app');
 
-const server = serverlessExpress.createServer(app);
+exports.handler = serverlessExpress({ app });
 
-exports.handler = (event, context) => {
-  // context.callbackWaitsForEmptyEventLoop = false;
+// const server = serverlessExpress.createServer(app);
 
-  return serverlessExpress.proxy(server, event, context)
-};
+// exports.handler = (event, context) => {
+//   // context.callbackWaitsForEmptyEventLoop = false;
+
+//   return serverlessExpress.proxy(server, event, context)
+// };
--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -3,31 +3,31 @@ var router = express.Router();
 
 // 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
 router.get('/', async function(req, res){
-  const promisePool = pool.promise();
+  // const promisePool = pool.promise();
 
-  const [rows, fields] = awaitpromisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1');
+  // const [rows, fields] = awaitpromisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1');
     
-    promisePool.query(
-      `SELECT
-        works.work_id,
-        works.title,
-        works.genru,
-        works.name,
-        works.image,
-        COUNT(*) AS COUNT
-      FROM reviews          
-      INNER JOIN works ON reviews.work_id = works.work_id
-      WHERE created_at BETWEEN '${ rows[0] }' - INTERVAL 7 DAY AND '${ rows[0] }'
-      GROUP BY reviews.work_id
-      ORDER BY COUNT DESC
-      LIMIT 3`, function(error, result){
+  //   promisePool.query(
+  //     `SELECT
+  //       works.work_id,
+  //       works.title,
+  //       works.genru,
+  //       works.name,
+  //       works.image,
+  //       COUNT(*) AS COUNT
+  //     FROM reviews          
+  //     INNER JOIN works ON reviews.work_id = works.work_id
+  //     WHERE created_at BETWEEN '${ rows[0] }' - INTERVAL 7 DAY AND '${ rows[0] }'
+  //     GROUP BY reviews.work_id
+  //     ORDER BY COUNT DESC
+  //     LIMIT 3`, function(error, result){
 
-        res.send({
-          "statusCode": 200,
-          "body": result
-        });
-      }
-    );
+  //       res.send({
+  //         "statusCode": 200,
+  //         "body": result
+  //       });
+  //     }
+  //   );
 });
 
 module.exports = router;
