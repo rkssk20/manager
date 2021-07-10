6607f18e49d2108391023feb6028f35f0b093473
ささき <castanie96@gmail.com>
2021-07-11 01:34:26 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -7,45 +7,27 @@ router.get('/', async function(req, res){
 
   const [rows] = await promisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1');
 
-  promisePool.query(`SELECT * FROM reviews WHERE created_at=${ rows[0].created_at }`, function(error, result){
-    res.send({
-      "statusCode": 200,
-      "body": result
-    })
-  })
+  promisePool.query(
+    `SELECT
+      works.work_id,
+      works.title,
+      works.genru,
+      works.name,
+      works.image,
+      COUNT(*) AS COUNT
+    FROM reviews          
+    INNER JOIN works ON reviews.work_id = works.work_id
+    WHERE reviews.created_at BETWEEN ${ rows[0].created_at } - INTERVAL 7 DAY AND ${ rows[0].created_at }
+    GROUP BY reviews.work_id
+    ORDER BY COUNT DESC
+    LIMIT 3`, function(error, result){
 
-
-  // .promise()
-  // .then((response) => {
-  //   promisePool.query(`SELECT * FROM reviews WHERE created_at='${ response[0].created_at }'`, function(error, result){
-  //     res.send({
-  //       "statusCode": 200,
-  //       "body": result
-  //     })
-  //   })
-  // })
-    // pool.query(
-    //   `SELECT
-    //     works.work_id,
-    //     works.title,
-    //     works.genru,
-    //     works.name,
-    //     works.image,
-    //     COUNT(*) AS COUNT
-    //   FROM reviews          
-    //   INNER JOIN works ON reviews.work_id = works.work_id
-    //   WHERE reviews.created_at BETWEEN ${ response[0].created_at } - INTERVAL 7 DAY AND ${ response[0].created_at }
-    //   GROUP BY reviews.work_id
-    //   ORDER BY COUNT DESC
-    //   LIMIT 3`, function(error, result){
-
-        // res.send({
-        //   "statusCode": 200,
-        //   "body": result
-        // });
-      // }
-    // );
-  // });
+      res.send({
+        "statusCode": 200,
+        "body": result
+      });
+    }
+  );
 });
 
 module.exports = router;
