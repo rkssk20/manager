ad55c749b69b47bcfb8453cbc1a0f12b28a1d4bc
ささき <castanie96@gmail.com>
2021-07-11 01:48:58 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 // 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
 router.get('/', async function(req, res){
-  // const promisePool = pool.promise();
+  const promisePool = pool.promise();
 
-  const [rows] = await pool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1');
+  const [response] = await promisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1');
 
-  pool.query(
+  const [result] = await promisePool.query(
     `SELECT
       works.work_id,
       works.title,
@@ -17,17 +17,16 @@ router.get('/', async function(req, res){
       COUNT(*) AS COUNT
     FROM reviews          
     INNER JOIN works ON reviews.work_id = works.work_id
-    WHERE reviews.created_at BETWEEN '${ rows[0].created_at }' - INTERVAL 7 DAY AND '${ rows[0].created_at }'
+    WHERE reviews.created_at BETWEEN '${ response[0].created_at }' - INTERVAL 7 DAY AND '${ response[0].created_at }'
     GROUP BY reviews.work_id
     ORDER BY COUNT DESC
-    LIMIT 3`, function(error, result){
-
-      res.send({
-        "statusCode": 200,
-        "body": result
-      });
-    }
+    LIMIT 3`
   );
+
+  res.send({
+    "statusCode": 200,
+    "body": result
+  });
 });
 
 module.exports = router;
