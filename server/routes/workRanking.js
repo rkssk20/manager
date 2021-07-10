9cbae60d2836bf0e02732c78d7213f239696c124
ささき <castanie96@gmail.com>
2021-07-10 18:22:34 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
-router.get('/', async function(req, res){
+router.get('/', function(req, res){
   const promisePool = pool.promise();
 
   function Query(){
@@ -26,7 +26,7 @@ router.get('/', async function(req, res){
       GROUP BY reviews.work_id
       ORDER BY COUNT DESC
       LIMIT 3`, function(error, result){
-        
+
         res.send({
           "statusCode": 200,
           "body": result
