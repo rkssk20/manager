852af006e58c89612c475b730aa81c116ef16a43
ささき <castanie96@gmail.com>
2021-07-10 19:27:17 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -2,32 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 // 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
-router.get('/', async function(req, res){
-  const promisePool = pool.promise();
+router.get('/', function(req, res){
+  pool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1', function(error, response){
+    
+    pool.query(
+      `SELECT
+        works.work_id,
+        works.title,
+        works.genru,
+        works.name,
+        works.image,
+        COUNT(*) AS COUNT
+      FROM reviews          
+      INNER JOIN works ON reviews.work_id = works.work_id
+      WHERE created_at BETWEEN '${ response[0] }' - INTERVAL 7 DAY AND '${ response[0] }'
+      GROUP BY reviews.work_id
+      ORDER BY COUNT DESC
+      LIMIT 3`, function(error, result){
 
-  const [rows, fields] = await promisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1');
-
-  promisePool.query(
-    `SELECT
-      works.work_id,
-      works.title,
-      works.genru,
-      works.name,
-      works.image,
-      COUNT(*) AS COUNT
-    FROM reviews          
-    INNER JOIN works ON reviews.work_id = works.work_id
-    WHERE created_at BETWEEN '${ rows[0] }' - INTERVAL 7 DAY AND '${ rows[0] }'
-    GROUP BY reviews.work_id
-    ORDER BY COUNT DESC
-    LIMIT 3`, function(error, result){
-
-      res.send({
-        "statusCode": 200,
-        "body": result
-      });
-    }
-  );
+        res.send({
+          "statusCode": 200,
+          "body": result
+        });
+      }
+    );
+  });
 });
 
 module.exports = router;
