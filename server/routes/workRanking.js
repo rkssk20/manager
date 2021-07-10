3b6a195492ace3faf5ac35aa89819f349cc2af51
ささき <castanie96@gmail.com>
2021-07-10 17:51:47 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 // 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
 router.get('/', function(req, res){
-  pool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1')
-  .then(response => {
+  pool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1', function(error, result){
+    
     pool.query(
       `SELECT
         works.work_id,
@@ -15,17 +15,18 @@ router.get('/', function(req, res){
         COUNT(*) AS COUNT
       FROM reviews          
       INNER JOIN works ON reviews.work_id = works.work_id
-      WHERE created_at BETWEEN '${ response[0] }' - INTERVAL 7 DAY AND '${ response[0] }'
+      WHERE created_at BETWEEN '${ result[0] }' - INTERVAL 7 DAY AND '${ result[0] }'
       GROUP BY reviews.work_id
       ORDER BY COUNT DESC
       LIMIT 3`, function(error, result){
+
         res.send({
           "statusCode": 200,
           "body": result
         });
       }
-    )
-  })
+    );
+  });
 });
 
 module.exports = router;
