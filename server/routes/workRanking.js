b4531741e00a4b6113bb3a127b3287f85bb3a878
ささき <castanie96@gmail.com>
2021-07-11 01:08:35 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -2,19 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 // 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
-router.get('/', function(req, res){
+router.get('/', async function(req, res){
   const promisePool = pool.promise();
 
-  promisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1')
-  .promise()
-  .then((response) => {
-    promisePool.query(`SELECT * FROM reviews WHERE created_at='${ response[0].created_at }'`, function(error, result){
-      res.send({
-        "statusCode": 200,
-        "body": result
-      })
+  const [rows] = await promisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1');
+
+  promisePool.query(`SELECT * FROM reviews WHERE created_at='${ rows[0].created_at }'`, function(error, result){
+    res.send({
+      "statusCode": 200,
+      "body": result
     })
   })
+
+
+  // .promise()
+  // .then((response) => {
+  //   promisePool.query(`SELECT * FROM reviews WHERE created_at='${ response[0].created_at }'`, function(error, result){
+  //     res.send({
+  //       "statusCode": 200,
+  //       "body": result
+  //     })
+  //   })
+  // })
     // pool.query(
     //   `SELECT
     //     works.work_id,
