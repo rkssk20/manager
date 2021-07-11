b57a65a052f2fa3462720bf952aa8eee6e52b708
ささき <castanie96@gmail.com>
2021-07-12 02:33:38 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-let response;
+var mysql = require('mysql2');
 
-pool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1', function(error, result){
+const MYSQL_HOST = process.env['MYSQL_HOST'];
+const MYSQL_USER = process.env['MYSQL_USER'];
+const MYSQL_PASSWORD = process.env['MYSQL_PASSWORD'];
+const MYSQL_DATABASE = process.env['MYSQL_DATABASE'];
+
+var connection = mysql.createConnection({
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE
+});
+
+connection.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1', function(error, result){
   response = result[0].created_at;
 });
 
 // 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
 router.get('/', async function(req, res){
-  pool.query(
+  connection.query(
     `SELECT
       works.work_id,
       works.title,
