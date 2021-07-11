5739dd04132576529f0aaea2af8e9be737080a37
ささき <castanie96@gmail.com>
2021-07-11 14:44:26 +0900

    update

--- server/app.js
+++ server/app.js
@@ -82,31 +82,30 @@ global.pool = pool;
 app.get('/api', (req, res) => {
   const promisePool = pool.promise();
   
-  promisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1', function(error, response){
-    if(!error){
-      promisePool.query(
-        `SELECT
-            works.work_id,
-            works.title,
-            works.genru,
-            works.name,
-            works.image,
-            COUNT(*) AS COUNT
-          FROM reviews
-          INNER JOIN works ON reviews.work_id = works.work_id
-          WHERE reviews.created_at BETWEEN '${ response[0].created_at }' - INTERVAL 7 DAY AND '${ response[0].created_at }'
-          GROUP BY reviews.work_id
-          ORDER BY COUNT DESC
-          LIMIT 3`, function(error, result){
-            res.send({
-              "statusCode": 200,
-              "body": result
-            }
-          );
-        }
-      );
-    }
-  });
+  promisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1')
+  .then(([response]) => {
+    pool.query(
+      `SELECT
+          works.work_id,
+          works.title,
+          works.genru,
+          works.name,
+          works.image,
+          COUNT(*) AS COUNT
+        FROM reviews
+        INNER JOIN works ON reviews.work_id = works.work_id
+        WHERE reviews.created_at BETWEEN '${ response[0].created_at }' - INTERVAL 7 DAY AND '${ response[0].created_at }'
+        GROUP BY reviews.work_id
+        ORDER BY COUNT DESC
+        LIMIT 3`, function(error, result){
+          res.send({
+            "statusCode": 200,
+            "body": result
+          }
+        );
+      }
+    );
+  })
 });
 
 app.get('/lalala', async (req, res) => {
