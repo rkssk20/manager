f7ca4f97433c97ca6b9e73dfcc59f063211874dc
ささき <castanie96@gmail.com>
2021-07-16 15:53:53 +0900

    update

--- server/routes/workRanking.js
+++ server/routes/workRanking.js
@@ -3,27 +3,36 @@ var router = express.Router();
 
 // 最新の投稿から一週間以内で、多くレビューされた作品TOP3を取得
 router.get('/', async function(req, res){
-  const promisePool = pool.promise();
+  async function Query(){
+    const response = await new Promise((resolve, reject) => {
+      promisePool.query('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1', function(err, res){
+        resolve(res);
+      });
+    });
 
-  const response = await promisePool.execute('SELECT created_at FROM reviews ORDER BY created_at DESC LIMIT 1');
+    const result = await new Promise((resolve, reject) => {
+      promisePool.query(
+        `SELECT
+          works.work_id,
+          works.title,
+          works.genru,
+          works.name,
+          works.image,
+          COUNT(*) AS COUNT
+        FROM reviews
+        INNER JOIN works ON reviews.work_id = works.work_id
+        WHERE reviews.created_at BETWEEN '${ response[0].created_at }' - INTERVAL 7 DAY AND '${ response[0].created_at }'
+        GROUP BY reviews.work_id
+        ORDER BY COUNT DESC
+        LIMIT 3`, function(err, res){
+          resolve(res);
+      });
+    });
 
-  const result = await promisePool.execute(
-    `SELECT
-      works.work_id,
-      works.title,
-      works.genru,
-      works.name,
-      works.image,
-      COUNT(*) AS COUNT
-    FROM reviews
-    INNER JOIN works ON reviews.work_id = works.work_id
-    WHERE reviews.created_at BETWEEN '${ response[0].created_at }' - INTERVAL 7 DAY AND '${ response[0].created_at }'
-    GROUP BY reviews.work_id
-    ORDER BY COUNT DESC
-    LIMIT 3`
-  );
+    res.send(result);
+  };
 
-  res.send(result);
+  Query();
 });
 
 module.exports = router;
